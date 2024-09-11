21fad170ce881c0b300902e1a2f71a3ca81f24db
[Robin Srarf] <[robinsrarf@gmail.com]>
2024-09-11 16:37:26 +0530

    placed pagenotfound.jsx in component folder

--- TeamTrack/src/App.jsx
+++ TeamTrack/src/App.jsx
@@ -3,13 +3,13 @@ import "./App.css";
 import LandingPageComponent from "./Components/LandingPageComponent";
 import LoginPage from "./Components/LoginPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import PageNotfound from "./PageNotFound";
+import PageNotfound from "./Components/PageNotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPageComponent />,
-    errorElement: <PageNotfound/>
+    errorElement: <PageNotfound />,
   },
   { path: "/Login", element: <LoginPage /> },
 ]);

d40f41c1187808a38c7d52b79a1849e6fbb392c2
Robin Srarf <59559816+robinsrarf@users.noreply.github.com>
2024-09-13 19:42:03 +0530

    fixed css error

--- TeamTrack/tailwind.config.js
+++ TeamTrack/tailwind.config.js
@@ -4,16 +4,11 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-<<<<<<< HEAD
-        righteous: ["Righteous", "sans-serif"], // Add Righteous font
-      },
-=======
         righteous: ['Righteous', 'sans-serif'], // Add Righteous font
       }
     },
     backgroundImage: {
       'custom-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0.27) 22.1%, rgba(102, 102, 102, 0.27) 100%)',
->>>>>>> 03d6a2bd041f51d7c18404722b564af4fdf668ce
     },
   },
   plugins: [],
